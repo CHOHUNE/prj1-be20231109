c71ee696e2b1a8449abfc2465b2566f5923b0703
CHOHUNE <yuureru@gmail.com>
2023-11-09 15:20:40 +0900

    A minute ago 게시물 목록 응답, Board dto에 id, inserted 추가 sebaek

--- src/main/java/com/example/prj1be20231109/domain/Board.java
+++ src/main/java/com/example/prj1be20231109/domain/Board.java
@@ -3,9 +3,13 @@ package com.example.prj1be20231109.domain;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Board {
+    private Integer id;
     private String title;
     private String content;
     private String writer;
+    private LocalDate inserted;
 }

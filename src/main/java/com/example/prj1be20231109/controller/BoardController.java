9842a2a805ce4e0f644d7f308e09c606446c083c
CHOHUNE <yuureru@gmail.com>
2023-11-09 14:31:21 +0900

    저장 후 home으로

--- src/main/java/com/example/prj1be20231109/controller/BoardController.java
+++ src/main/java/com/example/prj1be20231109/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.example.prj1be20231109.controller;
 
-
+import com.example.prj1be20231109.domain.Board;
+import com.example.prj1be20231109.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
+
+    private final BoardService service;
+
     @PostMapping("add")
-    public void add() {
-        System.out.println("BoardController.add");
+    public ResponseEntity add(@RequestBody Board board) {
+        if (!service.validate(board)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (service.save(board)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
     }
-
-
 }
+
+
+
+
+
+
+

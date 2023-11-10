750eb3910a86fc036c665390272ebfb0cac66808
CHOHUNE <yuureru@gmail.com>
2023-11-10 10:23:57 +0900

    게시글 수정 기능

--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -1,10 +1,7 @@
 package com.example.prj1be20231109.mapper;
 
 import com.example.prj1be20231109.domain.Board;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -37,4 +34,14 @@ WHERE id=#{id}
     
 """)
     int deleteById(Integer id);
+
+    @Update("""
+
+UPDATE board
+SET title=#{title},
+content=#{content},
+writer=#{writer}
+WHERE id=#{id}
+""")
+    void update(Board board);
 }
